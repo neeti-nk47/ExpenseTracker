f2130a929d9a3f805f6eb851cf1f3244b8fe99a2
neeti-nk47 <neeti.nk47@gmail.com>
2023-06-15 14:38:22 +0530

    Added Signup/ login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignupLogin from "./components/SignupLogin";
+import Welcome from "./components/Welcome";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SignupLogin />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
